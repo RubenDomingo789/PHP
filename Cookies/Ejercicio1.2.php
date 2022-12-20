b1405dffe2caa23a1c63e9cf42de8c290ca0ce14
Rubén Domingo Robledo <77732760+RubenDomingo789@users.noreply.github.com>
2022-12-20 16:36:41 +0100

    Update Ejercicio1.2.php

--- Cookies/Ejercicio1.2.php
+++ Cookies/Ejercicio1.2.php
@@ -16,18 +16,38 @@
         <div style='background-color: <?= $_COOKIE['color'] ?>'>
             <p style="color: <?= $_COOKIE['letra'] ?>; font-family:<?= $_COOKIE['font'] ?>">Hola <?= $_COOKIE['nombre'] ?> <?= $_COOKIE['apellidos'] ?></p>
         </div>
+        <a href="Ejercicio1.php" <?php
+            setcookie('nombre', $_POST['nombre'], 0);
+            setcookie('apellidos', $_POST['apellidos'], 0);
+            setcookie('color', $_POST['color'], 0);
+            setcookie('letra', $_POST['letra'], 0);
+            setcookie('font', $_POST['font'], 0);
+            ?>>
+            Volver inicio
+        </a>
     <?php
     } else {
     ?>
         <div style='background-color: <?= $_POST['color'] ?>'>
             <p style="color: <?= $_POST['letra'] ?>; font-family:<?= $_POST['font'] ?>">Hola <?= $_POST['nombre'] ?> <?= $_POST['apellidos'] ?></p>
         </div>
-    <?php
+        <?php
         setcookie('nombre', $_POST['nombre']);
         setcookie('apellidos', $_POST['apellidos']);
         setcookie('color', $_POST['color']);
         setcookie('letra', $_POST['letra']);
         setcookie('font', $_POST['font']);
+        ?>
+        <a href="Ejercicio1.php" <?php
+            setcookie('nombre', $_POST['nombre'], 0);
+            setcookie('apellidos', $_POST['apellidos'], 0);
+            setcookie('color', $_POST['color'], 0);
+            setcookie('letra', $_POST['letra'], 0);
+            setcookie('font', $_POST['font'], 0);
+            ?>>
+            Volver inicio
+        </a>
+    <?php
     }
     ?>
 </body>
