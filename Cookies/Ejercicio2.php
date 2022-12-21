97ddb36b1c26858c67f6948569932b359f658a19
RubenDomingo789 <ruben.domingorobledo@gmail.com>
2022-12-21 08:48:26 +0100

    Gestion empleados

--- Cookies/Ejercicio2.php
+++ Cookies/Ejercicio2.php
@@ -7,21 +7,43 @@
 <body>
     <form method="post" action="">
         <p>Productos: </p>
-        <input type="checkbox" name="productos[]" value="0.60">Pan -> 0,60€</input><br>
-        <input type="checkbox" name="productos[]" value="2.20">Leche -> 2.20€</input><br>
-        <input type="checkbox" name="productos[]" value="2.50">Huevos -> 2.50€</input><br>
-        <input type="checkbox" name="productos[]" value="1.35">Lechuga -> 1.35€</input><br><br>
+        <input type="checkbox" name="productos[]" value="1">Pan -> 0,60€</input><br>
+        <input type="checkbox" name="productos[]" value="2">Leche -> 2.20€</input><br>
+        <input type="checkbox" name="productos[]" value="3">Huevos -> 2.50€</input><br>
+        <input type="checkbox" name="productos[]" value="4">Lechuga -> 1.35€</input><br><br>
         <input type="submit" name="botonEnviar" value="Enviar" />
     </form>
 
     <?php
-    if (isset($_POST['botonEnviar'])) {
+    if (isset($_POST['botonEnviar']) && isset($_POST['productos'])) {
         $producto = $_POST['productos'];
-        print_r($producto);
-        foreach ($_POST['productos'] as $clave) {
-            if ($producto[0])
-        setcookie('pan', $_POST['productos']);
+        $cont = 0;
+
+        foreach ($producto as $clave) {
+            switch ($clave) {
+                case 1:
+                    echo 'Has comprado pan';
+                    echo "<br> Precio: 0.60<br>";
+                    $cont = $cont + 0.60;
+                    break;
+                case 2:
+                    echo 'Has comprado leche';
+                    echo "<br> Precio: 2.20<br>";
+                    $cont = $cont + 2.20;
+                    break;
+                case 3:
+                    echo 'Has comprado huevos';
+                    echo "<br> Precio: 2.50<br>";
+                    $cont = $cont + 2.50;
+                    break;
+                case 4:
+                    echo 'Has comprado lechuga';
+                    echo "<br> Precio: 1.35<br>";
+                    $cont = $cont + 1.35;
+                    break;
+            }
         }
+        echo '<br>Precio de la compra: ' . $cont;
     }
     ?>
 </body>
